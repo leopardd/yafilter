bab9f053a7925169829718e42a80e4ca53acc10b
Nathachai Thongniran <inid3a@gmail.com>
2016-12-21 11:04:34 +0700

    Refactor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
+/*================================================================ Model
+ */
+
 /**
- * [Pixel description]
- * Value of all parameters is between 0-255
+ * Pixel
+ * value of all parameters is between 0-255
  * 
- * @param {number} r
- * @param {number} g
- * @param {number} b
- * @param {number} a
+ * @param {number} [r=0]
+ * @param {number} [g=0]
+ * @param {number} [b=0]
+ * @param {number} [a=1]
+ *
+ * @return {Pixel}
  */
 var Pixel = function(r, g, b, a) {
+  if (typeof r === 'undefined') r = 0;
+  if (typeof g === 'undefined') g = 0;
+  if (typeof b === 'undefined') b = 0;
+  if (typeof a === 'undefined') a = 1;
+
   return {
     red: r,
     green: g,
@@ -17,237 +27,346 @@ var Pixel = function(r, g, b, a) {
 };
 
 /**
- * [TwoDArray description]
+ * ImageDataArray
  *
- * @example createArray(2);
- * @example createArray(3, 2);
+ * ImageData
+ * {
+ *   data: Uint8ClampedArray
+ *   width: number
+ *   height: number
+ * }
  *
- * @see http://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
+ * ImageDataArray
+ * {
+ *   data: Array
+ *   width: number
+ *   height: number
+ * }
  * 
- * @param {number} length
+ * @param {Uint8ClampedArray} data
+ * @param {number} width
+ * @param {number} height
+ *
+ * @return {ImageDataArray}
  */
-var TwoDArray = function(length) {
-  var arr = new Array(length || 0),
-    i = length;
-
-  if (arguments.length > 1) {
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    while(i--) {
-      arr[length-1 - i] = TwoDArray.apply(this, args);
-    }
-  }
-
-  return arr;
+var ImageDataArray = function(data, width, height) {
+  return {
+    data: data,
+    width: width,
+    height: height
+  };
 };
 
-var ImgPro = function(imageData) {
+/**
+ * PixelPanels
+ * 
+ * @return {Array.Array.Pixel}
+ */
+var PixelPanels = function(width, height) {
+  return Util.create2DArrayWithDefault(width, height, new Pixel());
+};
 
-  /** @type {ImageData} */
-  this.originalImageData = imageData;
+/*================================================================ Static
+ */
 
-  /** @type {Uint8ClampedArray} */
-  this.imageData = new Uint8ClampedArray(this.originalImageData.data.length),
-
-  /** @type {number} */
-  this.width = this.originalImageData.width;
-
-  /** @type {number} */
-  this.height = this.originalImageData.height;
-
-  /** @type {TwoDArray} the formatted original data  */
-  this.data = new TwoDArray(this.width, this.height);
-
-  /** @type {TwoDArray} the formatted original data that has been passed some filter */
-  this.result = new TwoDArray(this.width, this.height);
+var Util = {
 
   /**
-   * [clone description]
+   * Creature 2D array
+   * 
+   * @see http://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
+   * 
+   * @param  {number} length
+   * 
+   * @return {Array.Array}
+   */
+  create2DArray: function(length) {
+    var arr = new Array(length || 0),
+    i = length;
+
+    if (arguments.length > 1) {
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      while(i--) {
+        arr[length-1 - i] = this.create2DArray.apply(this, args);
+      }
+    }
+
+    return arr;
+  },
+
+  /**
+   * Creature 2D array with default value
+   * 
+   * @see http://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
+   * 
+   * @param  {number} length
+   * 
+   * @return {Array.Array}
+   */
+  create2DArrayWithDefault: function(rows, cols, defaultValue) {
+    var arr = [],
+      i = 0,
+      j = 0;
+
+    for (i = 0; i < rows; i++) {
+      arr.push([]);
+      arr[i].push(new Array(cols));
+
+      for (j = 0; j < cols; j++) {
+        arr[i][j] = defaultValue;
+      }
+    }
+
+    return arr;
+  },
+
+  /**
    * Clone object
    * TODO: move to util class
    *
    * @see http://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
    * 
    * @param  {Object} obj
+   * 
    * @return {Object}
    */
-  this.clone = function(obj) {
+  clone: function(obj) {
     return JSON.parse(JSON.stringify(obj));
-  };
+  },
+};
+
+var Convert = {
 
   /**
-   * Init
-   * - Setup data
+   * ImageData to ImageDataArray
+   * 
+   * @param  {ImageData} imageData
+   * 
+   * @return {ImageDataArray}
    */
-  this.init = function() {
-    var i = 0,
-      j = 0;
-
-    for (i = 0; i < this.width; i++) {
-      for (j = 0; j < this.height; j++) {
-        var startIndex = j * this.width * 4 + i * 4,
-          pixel = new Pixel(
-            this.originalImageData.data[startIndex + 0],
-            this.originalImageData.data[startIndex + 1],
-            this.originalImageData.data[startIndex + 2],
-            this.originalImageData.data[startIndex + 3]
-          );
-
-        this.data[i][j] = pixel;
-      }
-    }
-
-    this.result = this.clone(this.data);
-  };
+  imageDataToImageDataArray: function(imageData) {
+    return (new ImageDataArray(
+      Array.from(imageData.data),
+      imageData.width,
+      imageData.height
+    ));
+  },
 
   /**
-   * Update ImageData from "this.result"
-   * used by `this.toImageData` only
-   */
-  this.updateImageData = function() {
-    var i = 0,
-      j = 0;
-
-    for (i = 0; i < this.width; i++) {
-      for (j = 0; j < this.height; j++) {
-        var startIndex = j * this.width * 4 + i * 4;
-
-        this.imageData[startIndex + 0] = this.result[i][j].red;
-        this.imageData[startIndex + 1] = this.result[i][j].green;
-        this.imageData[startIndex + 2] = this.result[i][j].blue;
-        this.imageData[startIndex + 3] = this.result[i][j].alpha;
-      }
-    }
-  };
-
-  /**
-   * Return ImageData from "this.data"
+   * ImageDataArray to ImageData
+   * 
+   * @param  {ImageDataArray} imageDataArray
    * 
    * @return {ImageData}
    */
-  this.toImageData = function() {
-    this.updateImageData();
-
-    return (new ImageData(this.imageData, this.width, this.height));
-  };
+  imageDataArrayToImageData: function(imageDataArray) {
+    return (new ImageData(
+      (new Uint8ClampedArray(imageDataArray.data)),
+      imageDataArray.width,
+      imageDataArray.height
+    ));
+  },
 
   /**
-   * [toGrayscale description]
+   * ImageData to PixelPanels
    * 
-   * @return {TwoDArray}
+   * @param  {ImageData} imageData
+   * 
+   * @return {PixelPanels}
    */
-  this.toGrayscale = function() {
+  imageDataToPixelPanels: function(imageData) {
     var i = 0,
-      j = 0;
+      j = 0,
+      pixelPanels = new PixelPanels(imageData.width, imageData.height);
 
-    for (j = 0; j < this.height; j++) {
-      for (i = 0; i < this.width; i++ ) {
-        var value = (0.21 * this.data[i][j].red) +
-          (0.71 * this.data[i][j].green) +
-          (0.08 * this.data[i][j].blue);
+    for (i = 0; i < imageData.width; i++) {
+      for (j = 0; j < imageData.height; j++) {
+        var startIndex = j * imageData.width * 4 + i * 4,
+          pixel = new Pixel(
+            imageData.data[startIndex + 0],
+            imageData.data[startIndex + 1],
+            imageData.data[startIndex + 2],
+            imageData.data[startIndex + 3]
+          );
 
-        this.result[i][j].red   = value;
-        this.result[i][j].green = value;
-        this.result[i][j].blue  = value;
-        this.result[i][j].alpha = this.data[i][j].alpha;
+        pixelPanels[i][j] = pixel;
       }
     }
 
-    return this.result;
-  };
+    return pixelPanels;
+  },
 
   /**
-   * [toInvert description]
+   * PixelPanels to ImageData
    * 
-   * @return {TwoDArray}
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {ImageData}
    */
-  this.toInvert = function() {
-    var i = 0,
+  pixelPanelsToImageData: function(pixelPanels) {
+    var width = pixelPanels.length,
+      height = pixelPanels[0].length,
+      data = new Uint8ClampedArray(width * height * 4),
+      i = 0,
       j = 0;
 
-    for (j = 0; j < this.height; j++) {
-      for (i = 0; i < this.width; i++ ) {
-        this.result[i][j].red   = 255 - this.data[i][j].red;
-        this.result[i][j].green = 255 - this.data[i][j].green;
-        this.result[i][j].blue  = 255 - this.data[i][j].blue;
-        this.result[i][j].alpha = this.data[i][j].alpha;
+    for (j = 0; j < height; j++) {
+      for (i = 0; i < width; i++) {
+        var startIndex = j * width * 4 + i * 4;
+
+        data[startIndex + 0] = pixelPanels[i][j].red;
+        data[startIndex + 1] = pixelPanels[i][j].green;
+        data[startIndex + 2] = pixelPanels[i][j].blue;
+        data[startIndex + 3] = pixelPanels[i][j].alpha;
       }
     }
 
-    return this.result;
-  };
+    return (new ImageData(data, width, height));
+  },
+};
+
+var Filter = {
+
+  /**
+   * Grayscale
+   * 
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
+   */
+  grayscale: function(pixelPanels) {
+    var width = pixelPanels.length,
+      height = pixelPanels[0].length,
+      result = new PixelPanels(width, height),
+      i = 0,
+      j = 0;
+
+    for (j = 0; j < height; j++) {
+      for (i = 0; i < width; i++ ) {
+        var value = (0.21 * pixelPanels[i][j].red) +
+          (0.71 * pixelPanels[i][j].green) +
+          (0.08 * pixelPanels[i][j].blue);
+
+        result[i][j] = new Pixel(value, value, value, pixelPanels[i][j].alpha);
+      }
+    }
+
+    return result;
+  },
+
+  /**
+   * Invert
+   * 
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
+   */
+  invert: function(pixelPanels) {
+    var width = pixelPanels.length,
+      height = pixelPanels[0].length,
+      result = new PixelPanels(width, height),
+      i = 0,
+      j = 0;
+
+    for (j = 0; j < height; j++) {
+      for (i = 0; i < width; i++ ) {
+        result[i][j] = new Pixel(
+          255 - pixelPanels[i][j].red,
+          255 - pixelPanels[i][j].green,
+          255 - pixelPanels[i][j].blue,
+          pixelPanels[i][j].alpha
+        );
+      }
+    }
+
+    return result;
+  },
 
   /**
    * Rotate 180deg
    * TODO: optimize logic
    * 
-   * @return {TwoDArray}
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
    */
-  this.toRotate = function() {
-    var i = 0,
+  rotate: function(pixelPanels) {
+    var width = pixelPanels.length,
+      height = pixelPanels[0].length,
+      result = new PixelPanels(width, height),
+      i = 0,
       j = 0;
 
-    for (j = 0; j < this.height; j++) {
-      for (i = 0; i < this.width; i++ ) {
-        this.result[i][j].red   = this.data[this.width - i - 1][this.height - j - 1].red;
-        this.result[i][j].green = this.data[this.width - i - 1][this.height - j - 1].green;
-        this.result[i][j].blue  = this.data[this.width - i - 1][this.height - j - 1].blue;
-        this.result[i][j].alpha = this.data[this.width - i - 1][this.height - j - 1].alpha;
+    for (j = 0; j < height; j++) {
+      for (i = 0; i < width; i++ ) {
+        result[i][j] = new Pixel(
+          pixelPanels[width - i - 1][height - j - 1].red,
+          pixelPanels[width - i - 1][height - j - 1].green,
+          pixelPanels[width - i - 1][height - j - 1].blue,
+          pixelPanels[width - i - 1][height - j - 1].alpha
+        );
       }
     }
 
-    return this.result;
-  };
+    return result;
+  },
 
   /**
    * Flip
    * TODO: optimize logic
    * 
-   * @return {TwoDArray}
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
    */
-  this.toFlip = function() {
-    var i = 0,
+  flip: function(pixelPanels) {
+    var width = pixelPanels.length,
+      height = pixelPanels[0].length,
+      result = new PixelPanels(width, height),
+      i = 0,
       j = 0;
 
-    for (j = 0; j < this.height; j++) {
-      for (i = 0; i < this.width; i++ ) {
-        this.result[i][j].red   = this.data[this.width - i - 1][j].red;
-        this.result[i][j].green = this.data[this.width - i - 1][j].green;
-        this.result[i][j].blue  = this.data[this.width - i - 1][j].blue;
-        this.result[i][j].alpha = this.data[this.width - i - 1][j].alpha;
+    for (j = 0; j < height; j++) {
+      for (i = 0; i < width; i++ ) {
+        result[i][j] = new Pixel(
+          pixelPanels[width - i - 1][j].red,
+          pixelPanels[width - i - 1][j].green,
+          pixelPanels[width - i - 1][j].blue,
+          pixelPanels[width - i - 1][j].alpha
+        );
       }
     }
 
-    return this.result;
-  };
+    return result;
+  },
 
   /**
-   * [filter3x3Pixel description]
-   * used by `this.filter3x3` only
+   * Filter3x3Pixel
+   * used by "this.filter3x3" only
+   * TODO: optimize logic
    * 
-   * @param {TwoDArray} data
-   * @param {TwoDArray} filter
+   * @param {PixelPanels} pixelPanels
+   * @param {Array.Array.number[3]} filter
    * @param {number} i index of width
    * @param {number} j index of height
    *
    * @return {Pixel}
    */
-  this.filter3x3Pixel = function(data, filter, i, j) {
-    var k = 0,
-      l = 0,
+  filter3x3Pixel: function(pixelPanels, filter, i, j) {
+    var k = 0, // width
+      l = 0, // height
       red = 0,
       green = 0,
       blue = 0,
       alpha = 0;
 
-    // height
     for (l = 0; l < 3; l++) {
-      // width
       for (k = 0; k < 3; k++) {
-        red   += filter[k][l] * this.data[i + k - 1][j + l - 1].red;
-        green += filter[k][l] * this.data[i + k - 1][j + l - 1].green;
-        blue  += filter[k][l] * this.data[i + k - 1][j + l - 1].blue;
-        alpha += filter[k][l] * this.data[i + k - 1][j + l - 1].alpha;
+        red   += filter[k][l] * pixelPanels[i + k - 1][j + l - 1].red;
+        green += filter[k][l] * pixelPanels[i + k - 1][j + l - 1].green;
+        blue  += filter[k][l] * pixelPanels[i + k - 1][j + l - 1].blue;
+        alpha += filter[k][l] * pixelPanels[i + k - 1][j + l - 1].alpha;
       }
     }
 
@@ -257,92 +376,115 @@ var ImgPro = function(imageData) {
       Math.round(blue),
       Math.round(alpha)
     ));
-  };
+  },
 
   /**
-   * [filter3x3 description]
+   * Filter3x3
    * TODO: fix the top and left border
    * 
-   * @param  {TwoDArray} filter size 3x3 only
+   * @param  {PixelPanels} pixelPanels
+   * @param  {Array.Array.number[3]} filter
    * 
-   * @return {TwoDArray}
+   * @return {PixelPanels}
    */
-  this.filter3x3 = function(filter) {
-    var i = 0,
+  filter3x3: function(pixelPanels, filter) {
+    var width = pixelPanels.length,
+      height = pixelPanels[0].length,
+      result = new PixelPanels(width, height),
+      i = 0,
       j = 0;
 
-    for (j = 1; j < this.height - 1; j++) {
-      for (i = 1; i < this.width - 1; i++ ) {
-        this.result[i][j] = this.filter3x3Pixel(this.data, filter, i, j);;
+    for (j = 1; j < height - 1; j++) {
+      for (i = 1; i < width - 1; i++ ) {
+        result[i][j] = this.filter3x3Pixel(pixelPanels, filter, i, j);;
       }
     }
 
-    return this.result;
-  };
+    return result;
+  },
+  
+  /**
+   * Blur
+   * 
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
+   */
+  blur: function(pixelPanels) {
+    return this.boxBlur(pixelPanels);
+  },
 
-  this.toBlur = function() {
-    return this.toBoxBlur();
-  };
-
-  this.toBoxBlur = function() {
+  /**
+   * Box blur
+   * 
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
+   */
+  boxBlur: function(pixelPanels) {
     var filter = [
       [1 / 9, 1 / 9, 1 / 9],
       [1 / 9, 1 / 9, 1 / 9],
       [1 / 9, 1 / 9, 1 / 9]
     ];
 
-    return this.filter3x3(filter);
-  };
+    return this.filter3x3(pixelPanels, filter);
+  },
 
-  this.toGaussianBlur = function() {
+  /**
+   * Gaussian blur
+   * 
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
+   */
+  gaussianBlur: function(pixelPanels) {
     var filter = [
       [1 / 16, 2 / 16, 1 / 16],
       [2 / 16, 4 / 16, 2 / 16],
       [1 / 16, 2 / 16, 1 / 16]
     ];
 
-    return this.filter3x3(filter);
-  };
+    return this.filter3x3(pixelPanels, filter);
+  },
 
-  this.toSharpen = function() {
+  /**
+   * Sharpen
+   * 
+   * @param  {PixelPanels} pixelPanels
+   * 
+   * @return {PixelPanels}
+   */
+  sharpen: function(pixelPanels) {
     var filter = [
       [0, -1, 0],
       [-1, 5, -1],
       [0, -1, 0]
     ];
 
-    return this.filter3x3(filter);
-  };
-
-  this.init();
+    return this.filter3x3(pixelPanels, filter);
+  },
 };
 
 self.onmessage = function(e) {
-  var imageData = JSON.parse(e.data.imageData),
-    boxId = e.data.boxId,
+  var pixelPanels = JSON.parse(e.data.pixelPanels),
     func = e.data.func,
-    msg = {};
+    filteredPixelPanels = Filter[func](pixelPanels);
+    msg = {
+      'pixelPanels' : JSON.stringify(filteredPixelPanels),
+      'func'        : func
+    };
 
-  imageData = new ImageData(
-    (new Uint8ClampedArray(imageData.data)),
-    imageData.width,
-    imageData.height
-  );
-
-  imgPro = new ImgPro(imageData);
-  imgPro[func]();
-  imageData = imgPro.toImageData();
-
-  // prepare data for sending
-  var arrayedImageData = {};
-  // need to be array
-  arrayedImageData['data'] = Array.from(imageData.data);
-  arrayedImageData['width'] = imageData.width;
-  arrayedImageData['height'] = imageData.height;
-
-  var msg = {
-    'imageData' : JSON.stringify(arrayedImageData),
-    'func'      : 'toGrayscale'
-  };
   self.postMessage(msg);
 };
+
+// trick for testing with mocha (in module pattern)
+// http://stackoverflow.com/questions/14205631/how-do-i-test-a-basic-javascript-file-with-mocha
+if (typeof module !== 'undefined' && module.exports != null) {
+  exports.Pixel = Pixel;
+  exports.ImageDataArray = ImageDataArray;
+  exports.PixelPanels = PixelPanels;
+  exports.Util = Util;
+  exports.Convert = Convert;
+  exports.Filter = Filter;
+}
